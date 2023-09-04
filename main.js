366eb128e2d591b1966d27b9fa39842f836e3a34
LucasT06 <lucas.m.tiemens@gmail.com>
2023-09-04 12:23:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-console.log("index.html werkt");
 
-let elDiceOne = document.getElementById('dice1');
-let elDiceTwo = document.getElementById('dice2');
-let elComeOut = document.getElementById('roll');
-
-elComeOut.onclick = function () {
-  rollDice();
-};
-
-function rollDice() {
-  let diceOne = Math.floor((Math.random() * 6) + 1);
-  let diceTwo = Math.floor((Math.random() * 6) + 1);
-
-  console.log(diceOne + ' ' + diceTwo);
-
-  for (let i = 1; i <= 6; i++) {
-    elDiceOne.classList.remove('show-' + i);
-    if (diceOne === i) {
-      elDiceOne.classList.add('show-' + i);
-    }
-  }
-
-  for (let k = 1; k <= 6; k++) {
-    elDiceTwo.classList.remove('show-' + k);
-    if (diceTwo === k) {
-      elDiceTwo.classList.add('show-' + k);
-    }
-  }
-
-  setTimeout(rollDice, 1000); // Call rollDice again after a 1000ms delay
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // Plaats hier je bestaande code, inclusief de toewijzing van de onclick-handler
+    console.log("index.html werkt");
+    let elDiceOne = document.getElementById('dice1');
+    let elDiceTwo = document.getElementById('dice2');
+    let elComeOut = document.getElementById('roll');
+  
+    elComeOut.onclick = function () {
+      rollDice();
+    };
+  
+    // Rest van je code
+    function rollDice() {
+        let diceOne = Math.floor((Math.random() * 6) + 1);
+        let diceTwo = Math.floor((Math.random() * 6) + 1);
+      
+        console.log(diceOne + ' ' + diceTwo);
+      
+        for (let i = 1; i <= 6; i++) {
+          elDiceOne.classList.remove('show-' + i);
+          if (diceOne === i) {
+            elDiceOne.classList.add('show-' + i);
+          }
+        }
+      
+        for (let k = 1; k <= 6; k++) {
+          elDiceTwo.classList.remove('show-' + k);
+          if (diceTwo === k) {
+            elDiceTwo.classList.add('show-' + k);
+          }
+        }
+      
+        setTimeout(rollDice, 1000); // Call rollDice again after a 1000ms delay
+      }
+      
+  });
+  
