9026d49bb0883fee57f22fd356c09ac8e56a10fb
duncanvandien <143510323+duncanvandien@users.noreply.github.com>
2023-09-12 13:54:28 +0200

    l

--- Project-Hoger-Lager-main/main.js
+++ Project-Hoger-Lager-main/main.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("index.html werkt");
+
+    
     let elDiceOne = document.getElementById('dice1');
     let elDiceTwo = document.getElementById('dice2');
-    let elComeOut = document.getElementById('roll');
+    let elHigher = document.getElementById('higher');
+    let elLower = document.getElementById('lower');
+    let elRoll = document.getElementById('roll');
 
-    elComeOut.onclick = function () {
+    elHigher.onclick = function () {
+        // Voeg hier de logica toe voor "Hoger" knop
+    };
+
+    elLower.onclick = function () {
+        // Voeg hier de logica toe voor "Lager" knop
+    };
+
+    elRoll.onclick = function () {
         rollDice();
     };
 
@@ -14,19 +26,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
         console.log(diceOne + ' ' + diceTwo);
 
-        for (let i = 1; i <= 6; i++) {
-            elDiceOne.classList.remove('show-' + i);
-            if (diceOne === i) {
-                elDiceOne.classList.add('show-' + i);
-            }
-        }
-
-        for (let k = 1; k <= 6; k++) {
-            elDiceTwo.classList.remove('show-' + k);
-            if (diceTwo === k) {
-                elDiceTwo.classList.add('show-' + k);
-            }
-        }
+        elDiceOne.className = 'dice show-' + diceOne;
+        elDiceTwo.className = 'dice show-' + diceTwo;
     }
-});
 
+    // JavaScript om de pop-up te openen en te sluiten (blijft hetzelfde)
+    const showPopupButton = document.getElementById('showPopup');
+    const popup = document.getElementById('popup');
+    const closePopupButton = document.getElementById('closePopup');
+
+    showPopupButton.addEventListener('click', () => {
+        popup.style.display = 'block';
+    });
+
+    closePopupButton.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+});
