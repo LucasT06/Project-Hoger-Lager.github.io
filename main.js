0ec5e07d03091e3544ca0a6cbd17d1854549cbcb
duncanvandien <143510323+duncanvandien@users.noreply.github.com>
2023-09-29 10:30:58 +0200

    csa

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
     console.log("index.html werkt");
 
+<<<<<<< Updated upstream
     let diceOne = document.querySelector('dice1');
     let DiceTwo = document.querySelector('dice2');
     let dicetre = document.querySelector('dice3');
@@ -35,6 +36,53 @@
 
 
     });
+=======
+    let DiceOne = document.getElementById('dice1');
+    let DiceTwo = document.getElementById('dice2');
+    let Dicetre = document.getElementById('dice3');
+    let Dicefore = document.getElementById('dice4');
+    let Higher = document.getElementById('higher');
+    let Lower = document.getElementById('lower');
+    let Roll = document.getElementById('roll');
+
+
+    
+    Roll.onclick = function () {
+        rollDice();
+    };
+
+    function rollDice() {
+        let diceOne = Math.floor((Math.random() * 6) + 1);
+        let diceTwo = Math.floor((Math.random() * 6) + 1);
+
+        console.log(diceOne + ' ' + diceTwo);
+
+        DiceOne.className = 'dice show-' + diceOne;
+        DiceTwo.className = 'dice show-' + diceTwo;
+    }
+
+    Higher.onclick = function () {
+        let dicetre = Math.floor((Math.random() * 6) + 1);
+        let dicefore = Math.floor((Math.random() * 6) + 1);
+
+        console.log(dicetre + ' ' + dicefore);
+
+        Dicetre.className = 'dice show-' + dicetre;
+        Dicefore.className = 'dice show-' + dicefore;
+
+        if (diceOne + diceTwo > dicetre + divefore){
+            result = console.log("win")
+        }
+        else if (diceone + diceTwo < dicetre + divefore){
+            result = console.log("lose")
+        }
+    };
+
+    elLower.onclick = function () {
+        let dicetre = Math.floor((Math.random() * 6) + 1);
+        let dicefore = Math.floor((Math.random() * 6) + 1);
+    };
+>>>>>>> Stashed changes
 
     // JavaScript om de pop-up te openen en te sluiten (blijft hetzelfde)
     const showPopupButton = document.querySelector('#showPopup');
