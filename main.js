31da34dc9f5dd22d8216737169b3ab45c411c1e5
duncanvandien <143510323+duncanvandien@users.noreply.github.com>
2023-10-04 15:51:54 +0200

    bijna klaar

--- main.js
+++ main.js
@@ -1,81 +1,98 @@
 
-console.log("index.html werkt");
+    console.log("index.html werkt");
 
-const dice1 = document.querySelector('.dice1-img')
-const dice2 = document.querySelector('.dice2-img')
-const dice3 = document.querySelector('.dice3-img')
-const dice4 = document.querySelector('.dice4-img')
-const higherBtn = document.querySelector('.higher').disabled = true;
-const lowerBtn = document.querySelector('.lower').disabled = true;
-const roll = document.querySelector('.roll').disabled = false;
-const test = document.querySelector('.test')
+    const dice1 = document.querySelector('.dice1-img')
+    const dice2 = document.querySelector('.dice2-img')
+    const dice3 = document.querySelector('.dice3-img')
+    const dice4 = document.querySelector('.dice4-img')
+    const higher = document.querySelector('.higher')
+    const lower = document.querySelector('.lower')
+    const roll = document.querySelector('.roll')
 
-let diceOne;
-let diceTwo;
-let dicetre;
-let dicefore;
-
-// muisklik hoger knop
-higherBtn.addEventListener('click', function () {
-
-    // random getal 
-    dicetre = Math.floor((Math.random() * 6) + 1);
-    dicefore = Math.floor((Math.random() * 6) + 1);
-
-    console.log(dicetre + ' ' + dicefore);
-
-    // plaatsjes veranderen voor dobelsteen
-    dice3.src = "./img/dice-" + dicetre + ".png";
-    dice4.src = "./img/dice-" + dicefore + ".png";
-
-    if (diceOne + diceTwo < dicetre + dicefore) {
-        console.log("win")
-    }
-    else if (diceOne + diceTwo > dicetre + dicefore) {
-        console.log("lose")
-    } 
-});
-
-lowerBtn.addEventListener("click", function () {
-    dicetre = Math.floor((Math.random() * 6) + 1);
-    dicefore = Math.floor((Math.random() * 6) + 1);
-
-    console.log(dicetre + ' ' + dicefore);
-
-    dice3.src = "./img/dice-" + dicetre + ".png";
-    dice4.src = "./img/dice-" + dicefore + ".png";
-
-    if (diceOne + diceTwo > dicetre + dicefore) {
-        console.log("win")
-    }
-    else if (diceOne + diceTwo < dicetre + dicefore) {
-        console.log("lose")
-    }
-});
-
-roll.addEventListener('click', function () {
-    diceOne = Math.floor((Math.random() * 6) + 1);
-    diceTwo = Math.floor((Math.random() * 6) + 1);
-
-    console.log(diceOne + ' ' + diceTwo);
+    let diceOne
+    let diceTwo
+    let dicetre
+    let dicefore
 
 
+    roll.addEventListener ('click', function(){
+        diceOne = Math.floor((Math.random() * 6) + 1);
+        diceTwo = Math.floor((Math.random() * 6) + 1);
+        
+        console.log(diceOne + ' ' + diceTwo);
 
-    dice1.src = "./img/dice-" + diceOne + ".png";
-    dice2.src = "./img/dice-" + diceTwo + ".png";
+        dice1.src = "./img/dice-" + diceOne + ".png";
+        dice2.src = "./img/dice-" + diceTwo + ".png";
+
+        roll.disabled = true;
+        higher.disabled = false;
+        lower.disabled = false;
+    });
 
 
-});
+    higher.addEventListener('click', function(){
+        dicetre = Math.floor((Math.random() * 6) + 1);
+        dicefore = Math.floor((Math.random() * 6) + 1);
 
-const showPopupButton = document.querySelector('.showPopup');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.close');
+        console.log(dicetre + ' ' + dicefore);
 
-showPopupButton.addEventListener('click', function () {
-    popup.style.display = 'block';
-});
+        let result2 = dicetre + dicefore;
 
-closePopupButton.addEventListener('click', function () {
-    popup.style.display = 'none';
-});
+        dice3.src = "./img/dice-" + dicetre + ".png";
+        dice4.src = "./img/dice-" + dicefore + ".png";
+
+        if (diceOne + diceTwo < dicetre + dicefore){
+            console.log("win")
+        }
+        else if (diceOne + diceTwo > dicetre + dicefore){
+            console.log("lose")
+        }
+        else if (diceOne + diceTwo === dicetre + dicefore){
+            console.log('draw')
+        }
+
+        roll.disabled = false;
+        higher.disabled = true;
+        lower.disabled = true;
+
+    });
+
+    lower.addEventListener("click", function(){
+        dicetre = Math.floor((Math.random() * 6) + 1);
+        ldicefore = Math.floor((Math.random() * 6) + 1);
+
+        console.log(dicetre + ' ' + dicefore);
+
+        dice3.src = "./img/dice-" + dicetre + ".png";
+        dice4.src = "./img/dice-" + dicefore + ".png";
+
+        if (diceOne + diceTwo > dicetre + dicefore){
+            console.log("win")
+        }
+        else if (diceOne + diceTwo < dicetre + dicefore){
+            console.log("lose")
+        }
+        else if (diceOne + diceTwo === dicetre + dicefore){
+            console.log('draw')}
+
+        roll.disabled = false;
+        higher.disabled = true;
+        lower.disabled = true;
+    });
+
+    higher.disabled = true;
+    lower.disabled = true;
+
+
+    const showPopupButton = document.querySelector('.showPopup');
+    const popup = document.querySelector('.popup');
+    const closePopupButton = document.querySelector('.close');
+
+    showPopupButton.addEventListener('click', () => {
+        popup.style.display = 'block';
+    });
+
+    closePopupButton.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
 
